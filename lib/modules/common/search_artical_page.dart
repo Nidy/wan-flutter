9332e42cb020d415c225aabefeb8aca11cf1b806
YangJijun <yangjijun@petrochina.com.cn>
2020-07-09 17:42:25 +0800

    调整收藏与取消收藏的页面展示逻辑

---
+++ lib/modules/common/search_artical_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SearchArticalPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SearchArticalPageState();
+}
+
+class _SearchArticalPageState extends State<SearchArticalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/widget/artical_list_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ArticalListPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ArticalListPage();
+}
+
+class _ArticalListPage extends State<ArticalListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
